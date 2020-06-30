082b242c8de9be5a7a792494138059d40ea184b7
moussaddak <meddeb.moussaddak@gmail.com>
2020-06-30 00:05:48 +0000

    BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
+""" BaseModel Class prototype """
 import uuid
 from datetime import datetime
 
-"""BaseModel that defines all common attributes/methods for other classes"""
 
-class BaseModel():
-    """class defines all common attributes/methods for other classes"""
+class BaseModel:
+    """ BaseModel of all other classes """
+
     def __init__(self, *args, **kwargs):
         """ initialization of instance attribute
         id : unique id number of instance
         created_at : when an instance is created
         updated_at : when the last modification of an instance
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
-           for i, j in kwargs.items():
-                if i == "created_at" or i == "updated_at":
-                    self.__dict__[i] = datetime.strptime(j, "%Y-%m-%dT%H:%M:%S.%f")
-                elif i == '__class__':
-                    self.__class__.__name__ = j
-                else:
-                    setattr(self, i, j)
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                elif not key == '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -42,4 +43,3 @@ class BaseModel():
         d.update(created_at=self.created_at.isoformat())
         d.update(updated_at=self.updated_at.isoformat())
         return d
-
