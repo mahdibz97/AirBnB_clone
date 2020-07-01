4b20d7fe86b5567bed891972f6517125cfe0b92f
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-07-01 22:18:03 +0200

    test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,17 +9,13 @@ from models import storage
 
 class TestFileStorage(unittest.TestCase):
 
-    @classmethod
-    def create_instance(cls):
-        """create instance for the test"""
-        cls.data1 = FileStorage()
-        cls.data2 = storage.all()
-
     def test_isinstance_FileStorage(self):
         """test if the data is an type FileStorage"""
+        data1 = FileStorage()
+        data2 = storage.all()
 
-        self.assertTrue(isinstance(self.data1, FileStorage))
-        self.assertTrue(isinstance(self.data2, FileStorage))
+        self.assertTrue(isinstance(data1, FileStorage))
+        self.assertTrue(isinstance(data2, FileStorage))
 
 
 if __name__ == '__main__':
