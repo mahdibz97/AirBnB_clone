96f6d0909e24e5534a9b54782e43792c72405ba8
moussaddak <meddeb.moussaddak@gmail.com>
2020-07-01 18:09:41 +0000

    more class

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class Review """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Implementation of class Review """
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Class state """
+from models.base_model import BaseModel
+
+
+class state(BaseModel):
+    """ Implementation of class state """
+    name = ""
