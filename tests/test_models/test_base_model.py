37f1e6572ad4a703898289fb7292c0c1df17939b
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-07-01 22:16:50 +0200

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,16 +8,10 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
 
-    @classmethod
-    def set_up(cls):
-        """create instance for the test"""
-        cls.data = BaseModel()
-        cls.data.name = "Betty"
-        cls.data.num = 2020
-
     def test_isinstance_BaseModel(self):
         """test if the data is an type BaseModel"""
-
+        
+        data = BaseModel()
         self.assertTrue(isinstance(self.data, BaseModel))
 
     def test_docstring_check_BaseModel(self):
